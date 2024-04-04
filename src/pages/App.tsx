ac1a1c8a23f8f0ca1117a8e8b1afbc63d9a5cab1
Erick Dejo Vicente <61488294+mapachefurioso@users.noreply.github.com>
2024-04-03 23:43:53 -0500

    Agregado el listado de cartas

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,6 @@
-import { Banner, Title } from '../components'
-import '../styles/App.css'
+import { Banner, Card, Title } from '../components'
+import { recomendacionesData } from '../data'
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
 
@@ -8,6 +9,13 @@ function App() {
     <Banner buttonText="Explore More" titleText="Find more locations like this"></Banner>
      <section className="w-full h-screen mt-10">
       <Title title="Our Recommendations"></Title>
+      <div className='flex flex-row gap-4'>
+        {recomendacionesData.map((data,index)=>
+          <Card key={uuidv4()} tipo={index%2==0 ? 'claro' : 'oscuro'} imagen={data.imagen} subtitulo={data.subtitulo} titulo={data.titulo} ></Card>
+        )}
+        {/* <Card tipo='oscuro'></Card>
+        <Card tipo='claro'></Card> */}
+      </div>
      </section>
      </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,9 @@ export default {
   theme: {
     colors: {
       ...colors,
-      primary: "#F68E1E"
+      primary: "#F68E1E",
+      secondary: "#8FA206",
+      tertiary: "#CC2D4A",
     },
     fontFamily: {
       Roboto: ["Roboto", "sans-serif"],
@@ -16,13 +18,21 @@ export default {
     extend: {
       backgroundImage: {
         machupichu: "url('../assets/machupichu.jpg')",
+        catarata: "url('../assets/catarata.jpg')",
+        cuscocatedral: "url('../assets/cuscocatedral.jpg')",
+        huacachinalaguna: "url('../assets/huacachinalaguna.jpg')",
+        limaplaza: "url('../assets/limaplaza.jpg')",
       },
       backgroundColor: (theme) => ({
         ...theme("colors"),
-        primary: "#F68E1E"
+        primary: "#F68E1E",
+        secondary: "#8FA206",
+        tertiary: "#CC2D4A",
       }),
       textColor: {
-        primary: "#F68E1E"
+        primary: "#F68E1E",
+        secondary: "#8FA206",
+        tertiary: "#CC2D4A",
       },
     }
   },
