2136f9061f2dae1540f35cfaf8ecdbc67740de28
Erick Dejo Vicente <61488294+mapachefurioso@users.noreply.github.com>
2024-04-09 18:37:40 -0500

    Agregado el componente Section

---
+++ src/components/Section.tsx
@@ -0,0 +1,9 @@
+import { ReactElement } from "react";
+
+interface SectionProps {
+  children?: ReactElement | ReactElement[];
+}
+
+export const Section = ({ children }: SectionProps) => {
+  return <section className="w-full  my-10 px-16">{children}</section>;
+};
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,4 +1,12 @@
-import { Banner, Card, FAQForm, Grid, Paragraph, Title } from "../components";
+import {
+  Banner,
+  Card,
+  FAQForm,
+  Grid,
+  Paragraph,
+  Section,
+  Title,
+} from "../components";
 import { FAQData, recomendacionesData } from "../data";
 import { v4 as uuidv4 } from "uuid";
 
@@ -10,7 +18,7 @@ function App() {
         titleText="Find more locations like this"
         imgPath="machupichu.jpg"
       ></Banner>
-      <section className="w-full  mt-10 pl-6 pr-6 ">
+      <Section>
         <Title title="Our Recommendations"></Title>
         <div className="flex flex-row gap-4  pb-6 overflow-x-auto">
           {recomendacionesData.map((data, index) => (
@@ -22,30 +30,29 @@ function App() {
               titulo={data.titulo}
             ></Card>
           ))}
-          {/* <Card tipo='oscuro'></Card>
-        <Card tipo='claro'></Card> */}
         </div>
-      </section>
-      <section className="w-full  mt-10 pl-6 pr-6 ">
+      </Section>
+
+      <Section>
         <Title title="Trending Stays"></Title>
         <Grid></Grid>
-      </section>
-      <section className="w-full  mt-10 pl-6 pr-6 ">
+      </Section>
+      <Section>
         <FAQForm title="FAQ">
           {FAQData.map((itemText) => (
             <Paragraph type={itemText.type} text={itemText.text}></Paragraph>
           ))}
         </FAQForm>
-      </section>
-      <footer className="w-full h-auto bg-secondary p-6 mt-8  space-y-2 " >
-				<p className="text-lg text-white">Acerca de</p>
-				<p className="text-sm text-white">Política de Privacidad</p>
-				<p className="text-sm text-white">versionistas</p>
-				<p className="text-sm text-white">Terminos y Condiciones</p>
-				<p className="text-sm text-white">Empleos</p>
-				<p className="text-sm text-white">Platzi Travel,Inc</p>
-				<p className="text-sm text-white">Síguenos</p>
-			</footer>
+      </Section>
+      <footer className="w-full h-auto bg-secondary px-6 py-20 mt-8  space-y-2 ">
+        <p className="text-lg text-white">Acerca de</p>
+        <p className="text-sm text-white">Política de Privacidad</p>
+        <p className="text-sm text-white">versionistas</p>
+        <p className="text-sm text-white">Terminos y Condiciones</p>
+        <p className="text-sm text-white">Empleos</p>
+        <p className="text-sm text-white">Platzi Travel,Inc</p>
+        <p className="text-sm text-white">Síguenos</p>
+      </footer>
     </>
   );
 }
