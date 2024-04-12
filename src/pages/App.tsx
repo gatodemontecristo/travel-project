cea539999acfbdca1d46cb36fc8b5a269add8f9e
Erick Dejo Vicente <61488294+mapachefurioso@users.noreply.github.com>
2024-04-12 17:19:53 -0500

    Finalizado el menu footer responsive

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -17,6 +17,7 @@ import {
   MoonIcon,
   UserIcon,
 } from "@heroicons/react/24/solid";
+import { HeartIcon, HomeModernIcon, UserCircleIcon } from "@heroicons/react/16/solid";
 
 function App() {
   useEffect(() => {
@@ -119,6 +120,35 @@ function App() {
           <Paragraph type={itemText.type} text={itemText.text}></Paragraph>
         ))}
       </footer>
+      <section  className="flex md:hidden flex-row justify-evenly items-center fixed left-0 bottom-0 w-full h-16 border-solid border-t-2 border-gray-200 dark:border-gray-400 bg-white dark:bg-gray-800 transition-all duration-300">
+      <ButtonHead color="text-primary dark:text-white">
+              <HomeModernIcon className="h-7 w-7 fill-curren" />
+      </ButtonHead>
+
+      <ButtonHead color="text-primary dark:text-white">
+              <MagnifyingGlassIcon className="h-7 w-7 fill-curren" />
+      </ButtonHead>
+
+      <ButtonHead color="text-primary dark:text-white">
+              <HeartIcon className="h-7 w-7 fill-curren" />
+      </ButtonHead>
+
+      <ButtonHead color="text-primary dark:text-white">
+              <UserCircleIcon className="h-7 w-7 fill-curren" />
+      </ButtonHead>
+
+      {/* <a href="#recommendations">
+        <svg width="30" height="30" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path className="fill-current text-primary dark:text-white transition-all duration-300" fill-rule="evenodd" clip-rule="evenodd" d="M3.172 5.17202C3.92211 4.42214 4.93934 4.00087 6 4.00087C7.06066 4.00087 8.07789 4.42214 8.828 5.17202L10 6.34302L11.172 5.17202C11.541 4.78998 11.9824 4.48525 12.4704 4.27561C12.9584 4.06598 13.4833 3.95563 14.0144 3.95102C14.5455 3.9464 15.0722 4.04761 15.5638 4.24873C16.0554 4.44986 16.502 4.74687 16.8776 5.12244C17.2532 5.49801 17.5502 5.94462 17.7513 6.4362C17.9524 6.92779 18.0536 7.45451 18.049 7.98562C18.0444 8.51674 17.934 9.04162 17.7244 9.52964C17.5148 10.0177 17.21 10.459 16.828 10.828L10 17.657L3.172 10.828C2.42212 10.0779 2.00085 9.06068 2.00085 8.00002C2.00085 6.93936 2.42212 5.92213 3.172 5.17202Z"></path>
+        </svg>
+      </a>
+
+      <button>
+        <svg width="30" height="30" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path className="fill-current text-primary dark:text-white transition-all duration-300" fill-rule="evenodd" clip-rule="evenodd" d="M10 9C10.7956 9 11.5587 8.68393 12.1213 8.12132C12.6839 7.55871 13 6.79565 13 6C13 5.20435 12.6839 4.44129 12.1213 3.87868C11.5587 3.31607 10.7956 3 10 3C9.20435 3 8.44129 3.31607 7.87868 3.87868C7.31607 4.44129 7 5.20435 7 6C7 6.79565 7.31607 7.55871 7.87868 8.12132C8.44129 8.68393 9.20435 9 10 9ZM3 18C3 17.0807 3.18106 16.1705 3.53284 15.3212C3.88463 14.4719 4.40024 13.7003 5.05025 13.0503C5.70026 12.4002 6.47194 11.8846 7.32122 11.5328C8.1705 11.1811 9.08075 11 10 11C10.9193 11 11.8295 11.1811 12.6788 11.5328C13.5281 11.8846 14.2997 12.4002 14.9497 13.0503C15.5998 13.7003 16.1154 14.4719 16.4672 15.3212C16.8189 16.1705 17 17.0807 17 18H3Z"></path>
+        </svg>
+      </button> */}
+    </section>
     </div>
   );
 }
